406c550cbaaf06cd0dc1f3146c66adec7872ea9a
Varun Reddy Regalla <Varunreddy.Regalla@ad.infosys.com>
2016-06-28 18:49:59 +0530

    test

---
+++ js/script.js
@@ -0,0 +1,55 @@
+$(document).ready(function(){
+	
+	// $("#start-location").geocomplete().bind("geocode:result",function(event,result){
+	// 	var startLatitude=result.geometry.location.lat();
+	// 	var startLongitude=result.geometry.location.lng();
+		
+	// });
+
+	// $("#end-location").geocomplete().bind(function(event,result){
+	// 	var endLatitude=result.geometry.location.lat();
+	// 	var endLongitude=result.geometry.location.lng();
+		
+	// });
+	
+	// console.log(startLatitude,startLongitude,endLatitude,endLongitude);
+
+	var startLatitude=0;
+	var startLongitude=0;
+	
+	$("#start-location").geocomplete({
+		details:'.details-start',
+		detailsAttribute:'data-geo-start'
+	});
+
+	
+
+	$("button").click(function(){
+		startLatitude=$("#start-latitude").html();
+		startLongitude=$("#start-longitude").html();
+		
+
+		console.log(startLatitude,startLongitude);
+		getEstimatesForUserLocation(startLatitude,startLongitude);
+
+		function getEstimatesForUserLocation(latitude,longitude) {
+
+  		
+  		$.ajax({
+    		url: "api.openweathermap.org/data/2.5/forecast/daily?lat={lat}&lon={lon}&cnt={cnt}",
+    		type:'GET',
+    		datatype:"jsonp",
+    		data: {
+        		lat:startLatitude,
+        		lon: startLongitude,
+        		cnt:7
+        		},
+    		success: function(result) {
+        	console.log(result);
+    		}
+  		});
+		}
+		
+
+	});
+});
